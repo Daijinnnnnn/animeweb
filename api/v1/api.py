3b913d275fbe521ebd3a92669d202e2184afe8e5
Daijin <safory501@gmail.com>
2026-07-07 00:40:42 +0300

    Add files via upload

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,57 +1,72 @@
 from fastapi import APIRouter, HTTPException, Request
-import httpx
+import httpx 
 from typing import List
 from fastapi import Depends
 from schemas import AnimeResponse, UserFavoriteResponse
-from api.main import state, JIKAN_URL
+from main import state, JIKAN_URL
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_db
 from sqlalchemy import select
 from models import Anime, UserAnimeList, User
 from sqlalchemy.orm import selectinload, joinedload
+from auth.service import get_current_user_id
 
 
-app = APIRouter(prefix="/anime", tags=["Anime"])
+router = APIRouter(prefix="/anime", tags= ["Anime"])
 
 
-@app.get("/popular", response_model=List[AnimeResponse], response_model_by_alias=False)
+@router.get("/popular", response_model=List[AnimeResponse], response_model_by_alias=False)
 async def get_popular_anime():
     client: httpx.AsyncClient = state["http_client"]
 
+
     try:
-        response = await client.get(JIKAN_URL + "top/anime", params={"limit": 10})
+
+        response = await client.get(JIKAN_URL + "top/anime", params = {"limit": 10})
+    
+
         response.raise_for_status()
+
+
         full_response = response.json()
         anime_list = full_response.get("data", [])
+
         return anime_list
+
 
     except httpx.HTTPStatusError as exc:
         raise HTTPException(
-            status_code=exc.response.status_code,
-            detail=f"Jikan API вернул ошибку: {exc.response.text}",
+            status_code= exc.response.status_code,
+            detail=f'Jikan API вернул ошибку: {exc.response.text}'
         )
-
+    
     except httpx.RequestError:
         raise HTTPException(
-            status_code=503, detail="Сервис MyAnimeList (Jikan) временно недоступен"
+            status_code=503,
+            detail="Сервис MyAnimeList (Jikan) временно недоступен"
         )
 
 
-@app.get("/search", response_model=List[AnimeResponse], response_model_by_alias=False)
+@router.get("/search", response_model=List[AnimeResponse], response_model_by_alias=False)
 async def search_anime(query: str, request: Request, db: AsyncSession = Depends(get_db)):
     client: httpx.AsyncClient = state["http_client"]
 
     try:
+
         stmt = select(Anime).where(Anime.name_anime.ilike(f"%{query}%")).limit(10)
         result = await db.execute(stmt)
         cached_anime = result.scalars().all()
         if cached_anime:
             print("Данные взяты из кэша")
-            return cached_anime
+            return (cached_anime)
 
-        response = await client.get(JIKAN_URL + "anime", params={"q": query, "limit": 10})
+
+        response = await client.get(JIKAN_URL + "anime", params = {"q": query, "limit": 10})
+
         response.raise_for_status()
+
         full_response = response.json()
+
         anime_list = full_response.get("data", [])
 
         for item in anime_list:
@@ -59,81 +74,91 @@ async def search_anime(query: str, request: Request, db: AsyncSession = Depends(
             anime_exists = await db.get(Anime, anime_id)
 
             if not anime_exists:
-                new_anime = Anime(id=anime_id, name_anime=item.get("title"))
+                new_anime = Anime(
+                    id=anime_id,
+                    name_anime=item.get('title')
+                    )
                 db.add(new_anime)
-
+        
         await db.commit()
 
         return anime_list
-
+    
     except httpx.HTTPStatusError as exc:
         raise HTTPException(
-            status_code=exc.response.status_code,
-            detail=f"Jikan API вернул ошибку: {exc.response.text}",
+            status_code= exc.response.status_code,
+            detail=f'Jikan API вернул ошибку: {exc.response.text}'
         )
-
+    
     except httpx.RequestError:
         raise HTTPException(
-            status_code=503, detail="Сервис MyAnimeList (Jikan) временно недоступен"
+            status_code=503,
+            detail="Сервис MyAnimeList (Jikan) временно недоступен"
         )
 
 
-@app.post("/favorite")
-async def add_favorite(
-    user_id: int, anime_id: int, status: str, db: AsyncSession = Depends(get_db)
-):
-
+@router.post("/favorite")
+async def add_favorite(anime_id: int, status: str, db: AsyncSession = Depends(get_db),user_id: int = Depends(get_current_user_id)):
+    
     try:
         anime_exists = await db.get(Anime, anime_id)
         if anime_exists == None:
-            raise HTTPException(status_code=404, detail="Аниме не закэшировано")
-
-        new_favorite = UserAnimeList(user_id=user_id, anime_id=anime_id, status=status)
-
+            raise HTTPException(
+                status_code= 404,
+                detail= "Аниме не закэшировано"                               
+                                )
+        
+        new_favorite = UserAnimeList(
+        user_id = user_id,
+        anime_id = anime_id,
+        status = status     
+        )
+    
         db.add(new_favorite)
         await db.commit()
         return "успешно добавлено в списки"
-
+        
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"ошибка при добавлении в избранное {str(e)}")
+        raise HTTPException(
+            status_code=500,
+            detail= f"ошибка при добавлении в избранное {str(e)}"
+            
+            )
+    
 
 
-@app.get("/user/{user_id}/favorite", response_model=List[UserFavoriteResponse])
-async def favorite_list(user_id: int, db: AsyncSession = Depends(get_db)):
+@router.get("/favorite", response_model = List[UserFavoriteResponse])
+async def favorite_list(user_id: int = Depends(get_current_user_id), db: AsyncSession = Depends(get_db)):
     try:
-        stms = (
-            select(User)
-            .where(User.id == user_id)
-            .options(selectinload(User.anime_associations).joinedload(UserAnimeList.anime))
-        )
+        stms = select(User).where(User.id == user_id).options(selectinload(User.anime_associations).joinedload(UserAnimeList.anime))
         result = await db.execute(stms)
         user = result.scalar_one_or_none()
 
+
         if user is None:
-            raise HTTPException(status_code=404, detail="Юзер в базе не найден")
+            raise HTTPException(status_code=404,detail='Юзер в базе не найден')
+        
 
         return user.anime_associations
-
+    
     except HTTPException:
         raise
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Ошибка при получении избранного {str(e)}")
+        raise HTTPException(status_code=500, detail= f"Ошибка при получении избранного {str(e)}" )
+    
 
 
-@app.delete("/user/{user_id}/favorite/{anime_id}")
-async def delete_favorite(user_id: int, anime_id: int, db: AsyncSession = Depends(get_db)):
+@router.delete("/favorite/{anime_id}")
+async def delete_favorite(anime_id: int, db:AsyncSession = Depends(get_db),user_id: int = Depends(get_current_user_id)):
     try:
-        stms = select(UserAnimeList).where(
-            UserAnimeList.user_id == user_id, UserAnimeList.anime_id == anime_id
-        )
+        stms = select(UserAnimeList).where(UserAnimeList.user_id == user_id, UserAnimeList.anime_id == anime_id)
         result = await db.execute(stms)
         favorite = result.scalar_one_or_none()
 
         if favorite is None:
-            raise HTTPException(
-                status_code=404, detail="Данное аниме не найдено в списке этого пользователя"
-            )
+            raise HTTPException(status_code=404,detail='Данное аниме не найдено в списке этого пользователя')
+
 
         await db.delete(favorite)
         await db.commit()
@@ -144,4 +169,5 @@ async def delete_favorite(user_id: int, anime_id: int, db: AsyncSession = Depend
         raise
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Ошибка при удалении из избранного {str(e)}")
+        raise HTTPException(status_code=500, detail= f"Ошибка при удалении из избранного {str(e)}")
+
