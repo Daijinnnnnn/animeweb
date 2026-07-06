7ae32bed02c8bef3f1c7d76cad6c965508bf8776
Daijin <safory501@gmail.com>
2026-07-07 00:37:20 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,27 +1,30 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-import httpx
-
-from api.v1.api import app as anime_router
+import httpx 
 
 
-state: dict[str, httpx.AsyncClient] = {}
+state = {}
 
 JIKAN_URL = "https://api.jikan.moe/v4/"
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app:FastAPI):
     timeout_config = httpx.Timeout(connect=2.0, read=10.0, write=None, pool=None)
 
-    async with httpx.AsyncClient(timeout=timeout_config) as client:
+
+    async with httpx.AsyncClient(timeout = timeout_config) as client:
         state["http_client"] = client
         yield
+
 
     state.clear()
 
 
 app = FastAPI(lifespan=lifespan)
 
+from api import router as anime_router
+from auth.service import router as service_router
 
 app.include_router(anime_router)
+app.include_router(service_router)
